99efbde5701ae749f05b7338e9de19dfdba976ee
Patrick Bruce <pabr2151@colorado.edu>
2023-04-01 14:54:11 -0600

    wa10 update

--- js/wa10.js
+++ js/wa10.js
@@ -12,26 +12,29 @@ const displayedImg = document.querySelector('.displayed-img');
 const overlay = document.querySelector('.overlay');
 const darkenBtn = document.querySelector('.dark');
 
-for (let i = 0; i < imageFilenames.length; i++) {
-    const newImage = document.createElement('img');
-    newImage.setAttribute('src', '/img/' + imageFilenames[i]);
-    newImage.setAttribute('alt', imageAlt[imageFilenames[i]]);
-    thumbBar.appendChild(newImage);
-    newImage.addEventListener('click', function() {
-      displayedImg.setAttribute('src', this.getAttribute('src'));
-      displayedImg.setAttribute('alt', this.getAttribute('alt'));
-    });
-  }
-  
+for (let i = 0; i < imageFilenames.length; i++){
+  const newImage = document.createElement('img');
+  newImage.setAttribute('src', '/img/' + imageFilenames[i]);
+  newImage.setAttribute('alt', imageAlt[imageFilenames[i]]);
+  thumbBar.appendChild(newImage);
 
-  darkenBtn.addEventListener('click', function() {
-    if (darkenBtn.getAttribute('class') === 'dark') {
-      darkenBtn.setAttribute('class', 'light');
-      darkenBtn.textContent = 'Lighten';
-      overlay.style.backgroundColor = 'rgba(0,0,0,0.5)';
-    } else {
-      darkenBtn.setAttribute('class', 'dark');
-      darkenBtn.textContent = 'Darken';
-      overlay.style.backgroundColor = 'rgba(0,0,0,0)';
-    }
+  newImage.addEventListener('click', function(){
+    displayedImg.setAttribute('src', this.getAttribute('src'));
+    displayedImg.setAttribute('alt', this.getAttribute('alt'));
   });
+}
+
+  
+darkenBtn.addEventListener('click', function(){
+  if (darkenBtn.getAttribute('class') === 'dark'){
+    darkenBtn.setAttribute('class', 'light');
+    darkenBtn.textContent = 'Lighten';
+    overlay.style.backgroundColor = 'rgba(0,0,0,0.5)';
+  }
+
+  else{
+    darkenBtn.setAttribute('class', 'dark');
+    darkenBtn.textContent = 'Darken';
+    overlay.style.backgroundColor = 'rgba(0,0,0,0)';
+  }
+});
